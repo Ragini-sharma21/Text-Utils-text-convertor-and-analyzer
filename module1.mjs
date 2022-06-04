fe9b7fb1d72f5f418a76381f5e4bacad88c64649
Ragini sharma <raginisharma6006845094@gmail.com>
2022-06-04 23:06:08 +0530

    textutils

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+import ui,{a,b,d} from './module2.mjs' //use curly braces when we want to export something which is not bydefault
+console.log(ui);
+console.log(a);
+console.log(b);
+console.log(d);
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a ="ragini";
+const b ="pawan";
+const c ="aarav";
+const d ="sarita";
+
+export default c;
+export {a};
+export {b};
+export {d};
